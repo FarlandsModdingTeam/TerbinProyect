84d3b0c3c3c05a3b856e70e0aa8fdd9e01cb2101
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-04-12 18:55:16 +0200

    Client

--- TerbinService/Program.cs
+++ TerbinService/Program.cs
@@ -1,8 +1,11 @@
 using System.IO.Pipes;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using TerbinLibrary;
 using TerbinLibrary.Communication;
+using TerbinLibrary.Execution;
 using TerbinLibrary.Id;
+using TerbinLibrary.Memory;
 using TerbinLibrary.Serialize;
 using TerbinService;
 // TODO: Instalar BepiEx.
@@ -22,26 +25,23 @@ async Task simulateClient()
 {
     await Task.Delay(1000);
 
-    // Console.WriteLine($"[Client] Tamaño bytes ({Marshal.SizeOf<PacketRequest>()})");
-    // StreamBytes.StructToBytes(cap).ToArray().Length
+    var communicator = new TerbinCommunicator(false);
+    var executor = new TerbinExecutor();
+    ExecutableDispatcher.RegisterFromAssembly(Assembly.GetExecutingAssembly());
 
-    var id = MiniID.NewS;
-    Console.WriteLine($"[Client] Id: {id}");
+    if (await communicator.Connect())
+    {
+        Console.WriteLine($"[Client] ¡Conectado!");
+    }
+    else
+    {
+        Console.WriteLine($"[Client] ¡Error de Conexion!");
+        return;
+    }
 
-    using var pipe = TerbinCommunicator.CreateClientPipe();
-    await pipe.ConnectAsync();
-    _ = manejerSends(pipe);
-    Console.WriteLine($"[Client] ¡Conectado!");
-
-    var writer = new StreamWriteStruct(pipe);
 
     while (true)
     {
-        while (!SimulateClient.Write)
-        {
-            await Task.Delay(500);
-        }
-
         Console.Write($"-------( Start )---------\n"+
             $"[Client] (byte de accion: 0 = Stop, 10 = CreateInstance, )\n"+
             $"[Client] Action -> ");
@@ -49,60 +49,34 @@ async Task simulateClient()
         Console.Write($"[Client] ({input}), {(CodeMethodsTerbinService)input}\n"+
             $"-------(  End  )---------\n");
 
-        byte[] menssaje = new byte["matenme".ToCharArray().Length * 2];
-        try
-        {
-            menssaje = Serialineitor.SerializeArray<char>("matenme".ToCharArray());
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"[Cliente] Excption => {e.Message}");
-        }
+        //byte[] menssaje = new byte["matenme".ToCharArray().Length * 2];
+        //try
+        //{
+        //    menssaje = Serialineitor.SerializeArray<char>("matenme".ToCharArray());
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine($"[Cliente] Excption => {e.Message}");
+        //}
 
-        var header = new Header(id);
-        var cap = new PacketRequest(pHead: header, pActionMethod: input, pPayload: menssaje/**/);
+        MemoryStream memo = new MemoryStream();
+        StreamWriter writer = new StreamWriter(memo);
+        writer.Write("matenme".ToCharArray());
 
-        await writer.WriteAsycn<PacketRequest>(cap);
+        var r = await communicator.Communicate(input, memo);
+
+        Console.WriteLine($"[Client] R (Action: {r.ActionMethod} Status: {r.Head.Status})");
 
         if (input == 0)
         {
             Console.WriteLine($"[Client] Desconexion elegida, esperando confirmación del servidor...");
 
-            // Esperamos a que manejerSends reciba el paquete
-            while (!SimulateClient.Disconnect)
-            {
-                await Task.Delay(100);
-            }
-
-            Console.WriteLine($"[Client] Confirmación recibida, cerrando pipe de forma segura.");
             break; // <-- Ahora sí nos vamos.
         }
-        SimulateClient.Write = false;
     }
 }
 
-// TODO: Adaptar y meter en Library.
-async Task manejerSends(NamedPipeClientStream pPipe)
+async Task recibe()
 {
-    var reader = new StreamReadStruct(pPipe);
-    while (true)
-    {
-        var r = await reader.ReadAsycn<PacketRequest>();
-        Console.WriteLine($"[Client] R (Action: {r.ActionMethod} Status: {r.Head.Status})");
 
-        if (r.ActionMethod == (byte)CodeTerbinProtocol.Stop &&
-            r.Head.Status == CodeStatus.Succes)
-        {
-            Console.WriteLine($"[Client] Desconexion recibida.");
-            SimulateClient.Disconnect = true; // <-- Avisamos al hilo principal
-            break;
-        }
-        SimulateClient.Write = true;
-    }
-}
-
-class SimulateClient
-{
-    public static bool Write = true;
-    public static bool Disconnect = false;
 }
