b254fa3327bf14f3f7d95aa7dd53d94e0fa21656
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-04-15 22:13:56 +0200

    Siempre quise hacer esto (BoolExtension)

---
+++ TerbinLibrary/BoolExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerbinLibrary;
+public static class BoolExtension
+{
+    public static sbyte ToSByte(this bool pBool)
+    {
+        return (sbyte)(pBool ? 1 : 0);
+    }
+    public static byte ToByte(this bool pBool)
+    {
+        return (byte)(pBool ? 1 : 0);
+    }
+    public static sbyte ToSByte(this bool? pBool)
+    {
+        return (sbyte)(pBool != null ? ToSByte(pBool.Value) : -1);
+    }
+
+    public static bool ToBool(this sbyte pSByte)
+    {
+        return pSByte == 1;
+    }
+    public static bool ToBool(this byte pByte)
+    {
+        return pByte == 1;
+    }
+    public static bool? ToBoolUk(this sbyte pSByte)
+    {
+        return (pSByte == -1) ? null : ToBool(pSByte);
+    }
+}
