a2f04a8452b560c41507428479038f27809a03fb
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-04-15 23:46:59 +0200

    AcessJSon second version

--- TerbinLibrary/Data/AccessJSon.cs
+++ TerbinLibrary/Data/AccessJSon.cs
@@ -1,8 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Resources;
 using System.IO;
+using System.Numerics;
+using System.Resources;
 
 namespace TerbinLibrary.Data;
 
@@ -10,84 +11,89 @@ public class AcessJSon
 {
     private static Dictionary<string, string> _places = new Dictionary<string, string>();
 
-    public static string Get(string pKey)
+    public static string? Get(string pKey)
     {
         lock (_places)
         {
             if (_places.ContainsKey(pKey))
                 return _places[pKey];
         }
-        return "null";
+        return null;
     }
 
     public static void Set(string pKey, string pPlace)
     {
         lock (_places)
         {
-            if (!_places.ContainsKey(pKey))
-                _places.Add(pKey, pPlace);
-            else
-                _places[pKey] = pPlace;
+            _places[pKey] = pPlace;
         }
     }
 
-    public static sbyte Del(string pKey)
+
+    public enum CodeAcessJSonDel : sbyte
+    {
+        NotExistKey = -1,
+        Fail = 0,
+        Succes = 1,
+    }
+    public static CodeAcessJSonDel Del(string pKey)
     {
         lock (_places)
         {
             if (_places.ContainsKey(pKey))
-                return _places.Remove(pKey).ToSByte();
-            return -1;
+                return (CodeAcessJSonDel)_places.Remove(pKey).ToSByte();
+            return CodeAcessJSonDel.NotExistKey;
         }
     }
 
     public static T? Acess<T>(string pKey, string pFile) where T : class
     {
-        Dictionary<string, string> places;
-        lock (_places)
-        {
-            places = new(_places);
-            // places = [with(_places)]; // ¿Como que "preview?
-        }
+        string? dir = getDir(pKey);
+        if (dir == null)
+            return null;
 
         string fileName = getFileName(pFile);
 
-        if (places.TryGetValue(pKey, out var dir))
-        {
-            string routeComplete = Path.Combine(dir, fileName);
-            if (!File.Exists(routeComplete)) return null;
+        string routeComplete = Path.Combine(dir, fileName);
+        if (!File.Exists(routeComplete)) return null;
 
-            string json = File.ReadAllText(routeComplete);
-            if (json == null) return null;
+        string json = File.ReadAllText(routeComplete);
+        if (json == null) return null;
 
-            return JsonConvert.DeserializeObject<T>(json);
-        }
-        return null;
+        return JsonConvert.DeserializeObject<T>(json);
     }
 
-    public static sbyte Save<T>(string pKey, string pFile, T pContent) where T : class
+
+    public enum CodeAcessJSonSave : sbyte
     {
-        Dictionary<string, string> places;
+        ErrorSerialize = -1,
+        NotExistKey = 0,
+        Succes = 1,
+    }
+    public static CodeAcessJSonSave Save<T>(string pKey, string pFile, T pContent) where T : class
+    {
+        string? dir = getDir(pKey);
+        if (dir == null)
+            return CodeAcessJSonSave.NotExistKey;
+
+        string fileName = getFileName(pFile);
+
+        string routeComplete = Path.Combine(dir, fileName);
+
+        string json = JsonConvert.SerializeObject(pContent); // Formatting.Indented
+        if (json == null) return CodeAcessJSonSave.ErrorSerialize;
+
+        File.WriteAllText(routeComplete, json);
+        return CodeAcessJSonSave.Succes;
+    }
+
+    private static string? getDir(string pKey)
+    {
         lock (_places)
         {
-            places = new (_places);
-            // places = [with(_places)];
+            _places.TryGetValue(pKey, out var dir);
+            return dir;
         }
-
-        string fileName = getFileName(pFile); 
-
-        if (places.TryGetValue(pKey, out var dir))
-        {
-            string routeComplete = Path.Combine(dir, fileName);
-            if (!File.Exists(routeComplete)) return -1;
-
-            string json = JsonConvert.SerializeObject(pContent);
-            if (json == null) return -1;
-
-            File.WriteAllText(routeComplete, json);
-            return 1;
-        }
-        return 0;
     }
 
     private static string getFileName(string pFile)
@@ -95,5 +101,4 @@ public class AcessJSon
         return pFile.EndsWith(".json", StringComparison.OrdinalIgnoreCase)
                 ? pFile : pFile + ".json";
     }
-
 }
