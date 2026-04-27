db768e250febb19ec14c30146eb45f50e6f7c010
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-04-27 23:55:35 +0200

    todos los TODO de CreateInstance

--- TerbinService/Instances/CreateInstances.cs
+++ TerbinService/Instances/CreateInstances.cs
@@ -10,24 +10,40 @@ namespace TerbinService.Instances;
 
 public partial class InstancesService
 {
+    // El nombre de la instancia, bool si quieres instalar BepInEx, 
     [TerbinExecutableCompound((byte)CodeTerbinProtocol.Create, (byte)CodeSubServices.Instances)]
-    public static async Task<InfoResponse?> Create(Header pHead, byte[] pParameters)
+    public static async Task<InfoResponse?> CreateInstance(Header pHead, byte[] pParameters)
     {
-        Console.WriteLine($"[Worker] IdRequest: {pHead.IdRequest}");
-        Console.WriteLine($"[Worker] Largo: {pParameters.Length}");
-        if (pParameters.Length > 0)
-        {
-            char[] falseString = Serialineitor.DeserializeArray<char>(pParameters.ToArray());
-            Console.WriteLine($"[Worker] Mensaje: {new string(falseString)}");
-        }
-        else
-        {
-            Console.WriteLine("[Worker] Mensaje: (Sin payload)");
-        }
+        if (pParameters.Length <= 0)
+            return InfoResponse.Create(pHead.IdRequest, CodeStatus.ErrorNotPayload);
+
+        // TODO: De carpeta de Farlands.
+        // ├─Coger Dir Carpeta.
+        // └─Comprobar si esta el ejecutable <todo: mover dentro del metodo>.
+
+        // TODO: Del nombre carpeta destino.
+        // └─Comprobar si la carpeta esta vacia.
+
+        // HandleCreateInstance(); // Tengo sueño.
+        // HandleCloneFarlands();
+
+        // TODO: Instalar BepInEx si el entrante es que si.
 
         return InfoResponse.CreateSucces(pHead.IdRequest);
     }
 
+    public static async Task HandleCreateInstance()
+    {
+        // TODO: De la carpeta destino.
+        // ├─Comprobar si esta vacia.
+        // └─Comprobar si ya hay una instancia.
+    }
+    public static async Task HandleCloneFarlands()
+    {
+        // TODO: Clonar Farlands.
+        // ├─Crear Manifest.
+        // └─Guardar en Config la dir de la instancia.
+    }
 
     /// <summary>
     /// 
