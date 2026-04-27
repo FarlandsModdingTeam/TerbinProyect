9fbaeb6d98ea61425967af6a3feecdc7d29f17cb
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-04-27 21:35:37 +0200

    Update TerbinURLs.cs

--- TerbinLibrary/TerbinURLs.cs
+++ TerbinLibrary/TerbinURLs.cs
@@ -10,15 +10,20 @@ public class TerbinURLs
         "https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.3/BepInEx_win_x64_5.4.23.3.zip";
 
 
-    public const string TEST_200MB =
+    public const string TEST_200MB_ZIP =
         "http://ipv4.download.thinkbroadband.com/200MB.zip";
 
-    public const string TEST_512MB =
+    public const string TEST_512MB_ZIP =
         "http://ipv4.download.thinkbroadband.com/512MB.zip";
 
-    public const string TEST_1GB =
+    public const string TEST_1GB_ZIP =
         "http://ipv4.download.thinkbroadband.com/1GB.zip";
 
-    // "https://speed.hetzner.de/1GB.bin"
+
+    public const string TEST_1GB_BIN =
+        "https://speed.hetzner.de/1GB.bin"; // Deprecated, pero no hay que perderlo.
+
+    public const string TEST_10GB_BIN =
+        "https://nbg1-speed.hetzner.com/10GB.bin";
 }
 
