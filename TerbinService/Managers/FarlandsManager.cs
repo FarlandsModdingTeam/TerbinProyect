8db1f1186b274b9a1687e9df107ba3bbf07801f9
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-05-10 15:11:33 +0200

    Created voids Managers

---
+++ TerbinService/Managers/FarlandsManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerbinService.Managers;
+/*
+ -- Variables:
+  empieza: _ = es privada NO local.
+  empieza: minuscula = es privada local.
+  empieza: "p"en minuscula = parametro entrante local.
+  empieza: mayuscula = publica.
+ -- Funciones:
+  empieza: mayusculas = publica.
+  empieza: minusculas = privada.
+ */
+
+
+public static class FarlandsManager
+{
+
+}
