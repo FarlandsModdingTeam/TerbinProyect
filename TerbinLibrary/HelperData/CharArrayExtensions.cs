f77c2e26ebed40d45b820603b078a1e2808a22c4
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-06-06 13:48:02 +0200

    Refact Extension to Datahelper

---
+++ TerbinLibrary/HelperData/CharArrayExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TerbinLibrary.Extension;
+
+public static class CharArrayExtensions
+{
+    public static string CrString(this char[] array)
+    {
+        return new string(array);
+    }
+}
---
+++ TerbinLibrary/HelperData/ObjectExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerbinLibrary.HelperData;
+
+
+
+public static class ObjectExtension
+{
+    public static bool IsNull(this object pObj)
+    {
+        return pObj is null;
+    }
+}
