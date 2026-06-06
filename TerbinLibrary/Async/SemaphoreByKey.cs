a38951ad1db322b3e2c258a33398a10ae858335b
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-06-06 12:44:04 +0200

    improved SemaphoreByKey

--- TerbinLibrary/Async/SemaphoreByKey.cs
+++ TerbinLibrary/Async/SemaphoreByKey.cs
@@ -25,41 +25,54 @@ public class SemaphoreByKey<TKey> where TKey : notnull
 
     public SemaphoreByKey(IEqualityComparer<TKey>? pComparer = null)
     {
-        _locks = new ConcurrentDictionary<TKey, SemaphoreSlim>(pComparer ?? EqualityComparer<TKey>.Default);
+        this._locks = new ConcurrentDictionary<TKey, SemaphoreSlim>(pComparer ?? EqualityComparer<TKey>.Default);
     }
 
     /// <summary>
     /// Solicita el acceso exclusivo para una clave específica. 
     /// Devuelve un token ejecutable en un bloque 'using' que liberará el semáforo al destruirse.
     /// </summary>
-    public async Task<IDisposable> LockAsync(TKey pKey, CancellationToken pToken = default)
+    public async Task<IDisposable> LockAsync(TKey pK, CancellationToken pToken = default)
     {
-        var semaphore = _locks.GetOrAdd(pKey, _ => new SemaphoreSlim(1, 1));
+        var semaphore = _locks.GetOrAdd(pK, _ => new SemaphoreSlim(1, 1));
 
         await semaphore.WaitAsync(pToken).ConfigureAwait(false);
 
         return new Releaser(semaphore);
     }
 
+    public async Task<IDisposable> LockAsync(TKey pK1, TKey pK2, CancellationToken pToken = default)
+    {
+        var s1 = _locks.GetOrAdd(pK1, _ => new SemaphoreSlim(1, 1));
+        var s2 = _locks.GetOrAdd(pK2, _ => new SemaphoreSlim(1, 1));
 
+        await s1.WaitAsync(pToken).ConfigureAwait(false);
+        await s2.WaitAsync(pToken).ConfigureAwait(false);
+
+        return new Releaser(s1, s2);
+    }
 
 
     private sealed class Releaser : IDisposable
     {
-        private readonly SemaphoreSlim _semaphore;
+        private readonly SemaphoreSlim[] _semaphore;
         private int _isDisposed;
 
-        public Releaser(SemaphoreSlim pSemaphore)
+        public Releaser(SemaphoreSlim pSemaphore) : this([pSemaphore])
         {
-            _semaphore = pSemaphore;
+        }
+        public Releaser(params SemaphoreSlim[] pSemaphore)
+        {
+            this._semaphore = pSemaphore;
         }
 
         public void Dispose()
         {
             if (Interlocked.Exchange(ref _isDisposed, 1) == 0)
-            {
-                _semaphore.Release();
-            }
+                for (int i = 0; i < _semaphore.Length; i++)
+                {
+                    _semaphore[i].Release();
+                }
         }
     }
 }
