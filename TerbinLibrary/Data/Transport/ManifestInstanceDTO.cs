31f40b2424b0644940b2f9eee421f3a9ce91e4b1
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-06-08 04:01:57 +0200

    Create ManifestInstanceDTO.cs

---
+++ TerbinLibrary/Data/Transport/ManifestInstanceDTO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TerbinLibrary.Data.References;
+using TerbinLibrary.Extension;
+using TerbinLibrary.Protocol;
+using TerbinLibrary.Serialize;
+
+namespace TerbinLibrary.Data.Transport;
+
+public struct ManifestInstanceDTO : IStructSerializable
+{
+    public string? Name { get; set; }
+    public string? Version { get; set; }
+    public ThreeQuartersInt PluginCount { get; set; } = 0;
+
+    public ManifestInstanceDTO(string pName, string pVersion, ThreeQuartersInt pPlugins) : this(pName, pVersion)
+    {
+        this.PluginCount = pPlugins;
+    }
+    public ManifestInstanceDTO(string pName, string pVersion) : this(pName)
+    {
+        this.Version = pVersion;
+    }
+    public ManifestInstanceDTO(string pName)
+    {
+        this.Name = pName;
+    }
+    public ManifestInstanceDTO()
+    {
+
+    }
+
+    [TODO("Optimizar")]
+    public readonly int GetSize()
+    {
+        int size = ThreeQuartersInt.Size;
+
+        size += ((Name?.Length ?? 0) * 2) + TerbinProtocol.LENGTH_ARRAY;
+        size += ((Version?.Length ?? 0) * 2) + TerbinProtocol.LENGTH_ARRAY;
+
+        return size;
+    }
+
+    public void ReadFrom(ReadOnlySpan<byte> pBuffer)
+    {
+        int offset = 0;
+        Name = pBuffer.ReadArray<char>(ref offset).CrString();
+        Version = pBuffer.ReadArray<char>(ref offset).CrString();
+        PluginCount = pBuffer.Read<ThreeQuartersInt>(ref offset);
+    }
+
+    public readonly void WriteTo(Span<byte> pBuffer)
+    {
+        int offset = 0;
+        pBuffer.WriteArray<char>(ref offset, Name?.ToCharArray() ?? "".ToCharArray());
+        pBuffer.WriteArray<char>(ref offset, Version?.ToCharArray() ?? "".ToCharArray());
+        pBuffer.Write<ThreeQuartersInt>(ref offset, PluginCount);
+    }
+}
