1a86c1cf653cfa721b2e9b6a337c5339f76f0717
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-06-16 17:17:34 +0200

    Fix ServicePluginStorage, (anterior no era eso)

--- TerbinService/Services/ServicePluginStorage.cs
+++ TerbinService/Services/ServicePluginStorage.cs
@@ -18,7 +18,28 @@ namespace TerbinService.Services;
 
 internal class ServicePluginStorage
 {
+    // ReferencePluginStoreDTO
     [TerbinExecutable((byte)CodeServices.ReadAll, (byte)CodeServicesSection.PluginStorage)]
+    public static async Task<InfoResponse?> GetOne(Header pHead, byte[] pParameters, CancellationToken pToken)
+    {
+        List<ReferencePluginStore> plugin;
+        Serialineitor s = new();
+
+        plugin = await Manager.StoragePlugin.GetAll();
+
+        if (plugin.Count <= 0)
+            return InfoResponse.CreateSucces(pHead.IdRequest, [0]);
+
+        s.Add<ThreeQuartersInt>(plugin.Count);
+        for (int i = 0; i < plugin.Count; i++)
+            s.AddStruct<ReferencePluginStoreDTO>(plugin[i].ToDTO());
+
+        return InfoResponse.CreateSucces(pHead.IdRequest, s.Serialize());
+    }
+
+
+    // ReferencePluginStoreDTO
+    [TerbinExecutable((byte)CodeServices.Read, (byte)CodeServicesSection.PluginStorage)]
     public static async Task<InfoResponse?> GetAll(Header pHead, byte[] pParameters, CancellationToken pToken)
     {
         if (pParameters.Length <= 0)
@@ -34,30 +55,6 @@ internal class ServicePluginStorage
             return InfoResponse.CreateInteralError(pHead.IdRequest, TSHelper.GetError(CodeInternalErrors.PluginNotExist));
 
         return InfoResponse.CreateSucces(pHead.IdRequest, plugin.ToSerilizeDTO());
-    }
-
-    [TerbinExecutable((byte)CodeServices.Read, (byte)CodeServicesSection.PluginStorage)]
-    public static async Task<InfoResponse?> GetOne(Header pHead, byte[] pParameters, CancellationToken pToken)
-    {
-        //if (pParameters.Length <= 0)
-        //    return InfoResponse.Create(pHead.IdRequest, CodeStatus.ErrorNotPayload);
-
-        //ReadOnlySpan<byte> reader = pParameters;
-        //string id = reader.ReadArray<char>().CrString();
-
-        List<ReferencePluginStore> plugin;
-        Serialineitor s = new();
-
-        plugin = await Manager.StoragePlugin.GetAll();
-
-        if (plugin.Count <= 0)
-            return InfoResponse.CreateSucces(pHead.IdRequest, [0]);
-
-        s.Add<ThreeQuartersInt>(plugin.Count);
-        for (int i = 0; i < plugin.Count; i++)
-            s.AddStruct<ReferencePluginStoreDTO>(plugin[i].ToDTO());
-
-        return InfoResponse.CreateSucces(pHead.IdRequest, s.Serialize());
     }
 
 }
